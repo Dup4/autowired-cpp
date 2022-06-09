b4d8d02ec251043297d7ace47653a9362ce9c1e1
Dup4 <lyuzhi.pan@gmail.com>
2022-06-09 23:35:39 +0800

    refactor: fix project path

---
+++ include/autowired/autowired.h
@@ -0,0 +1,6 @@
+#ifndef AUTO_WIRED_AUTO_WIRED_H
+#define AUTO_WIRED_AUTO_WIRED_H
+
+class AutoWired {};
+
+#endif  // AUTO_WIRED_AUTO_WIRED_H
---
+++ test/autowired_test.cc
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(AUTO_WIRED, auto_wired) {
+    EXPECT_EQ(1, 1);
+}
